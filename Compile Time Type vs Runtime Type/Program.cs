220f5c908110d4e5cf61c4d76dffcfea7964c0c3
Kulpot <kulpotwahu@gmail.com>
2023-09-13 23:12:54 +0800

    Add project files.

---
+++ Compile Time Type vs Runtime Type/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=QmvmZqpthbc&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=54
+//
+
+class Animal { }
+class Dog : Animal { }
+class Cat : Animal { }
+
+class MainClass
+{
+    static void Main()
+    {
+        var randy = new Random();
+        bool randomBool = randy.Next() % 2 == 0;
+        Animal a = randomBool ? (Animal)new Dog() : (Animal)new Cat();
+        //Animal a = new Dog();
+        //Animal a = new Cat();
+        Dog d = (Dog)a; // cast for a for runtime of reference Dog // error for runtime reference cat because it is cast as Dog
+        while (true) ; // compile time loading slow
+    }
+}
