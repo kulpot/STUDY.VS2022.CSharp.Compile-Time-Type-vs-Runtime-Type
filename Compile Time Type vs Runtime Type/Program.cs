b7575217642c1bcd85905404df100dcb415d86d2
Kulpot <kulpotwahu@gmail.com>
2024-01-07 04:55:06 +0800

    ...

--- Compile Time Type vs Runtime Type/Program.cs
+++ Compile Time Type vs Runtime Type/Program.cs
@@ -12,11 +12,11 @@ class MainClass
     static void Main()
     {
         var randy = new Random();
-        bool randomBool = randy.Next() % 2 == 0; // for RNG between two subject
-        Animal a = randomBool ? (Animal)new Dog() : (Animal)new Cat();
+        bool randomBool = randy.Next() % 2 == 0; // random line code
+        Animal a = randomBool ? (Animal)new Dog() : (Animal)new Cat(); // runtime output: Dog ok, Cat casting error
         //Animal a = new Dog();
         //Animal a = new Cat();
         Dog d = (Dog)a; // cast for a for runtime of reference Dog // error for runtime reference cat because it is cast as Dog
-        while (true) ; // compile time loading slow
+        //while (true) ; // compile time loading slow
     }
 }
